6db37da75e8f1dde60366abebe632daad765c812
zhaoshiqi <2503642089@qq.com>
2022-09-01 14:31:09 +0800

    222222222222222222

--- src/main/java/com/atguigu/Demo021.java
+++ src/main/java/com/atguigu/Demo021.java
@@ -4,5 +4,6 @@ public class Demo021 {
     public static void main(String[] args) {
         System.out.println("haahahha");
         System.out.println("haahahha");
+        System.out.println("haahahha");
     }
 }
